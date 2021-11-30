d8682b80ea7966f9b54d46d8594c885f6dae3353
Peter Mwanzia <61682711+Peter-pixel@users.noreply.github.com>
2021-11-30 19:07:26 +0300

    Session recording

---
+++ pages/adminmenubar.php
@@ -0,0 +1,61 @@
+<?php
+    include "connect.php";
+    include "session_timeout.php";
+
+    /* Avoid multiple sessions warning
+    Check if session is set before starting a new one. */
+    if(!isset($_SESSION)) {
+        session_start();
+    }
+
+    if (isset($_SESSION['loggedIn_AdminId'])) {
+        $sql0 = "SELECT * FROM admin WHERE AdminId=".$_SESSION['loggedIn_AdminId'];
+        $result = $conn->query($sql0);
+        $row = $result->fetch_assoc();
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="user_navbar_style.css">
+    <link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+     <link href="search.css" rel="stylesheet" type="text/css" media="all">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body id="top">
+
+<div class="wrapper row1">
+  <header id="header" class="clear"> 
+  
+    <div id="logo" class="fl_left">
+      <h1><a href="adminhome.php">Lana</a></h1>
+    </div>
+    <nav id="mainav" class="fl_right">
+        <ul class="clear">
+     
+          <li class="active"><a href="adminhome.php">Home</a></li>
+              <li><a href="#">Announcements</a></li>
+              <li><a href="forum.php">Forum &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</a></li>
+              <li><a class="drop" href="#"><img alt="Profile" src="../images/demo/default-profile.png" style="width:40px;height:40px;"></a>
+              <ul>
+              <li><a id="logout" href="logout_action.php" onclick="return confirm('Are you sure?')">Logout</a></li>
+                 <li><a id="profile" href="adminprofile.php">My Profile</a></li>
+
+              </ul>
+            
+        </ul>
+   
+      
+      </nav>
+            <form class="example" action="search_action.php">
+  <input type="text" placeholder="Search..." name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+   
+  </header>
+</div>
+
+</body>
+</html>
---
+++ pages/forum.php
@@ -0,0 +1,69 @@
+<?php 
+include("connect.php");
+include("adminmenubar.php"); 
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Lana/forum</title>
+<meta charset="utf-8">
+<link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+
+
+<div class="wrapper row3">
+  <main id="container" class="clear"> 
+   
+    <div class="center push80">
+      <h1 class="shout">You can share your thoughts with other learners here.</h1>
+      
+    </div>
+    <iframe src='https://deadsimplechat.com/XqAYOwJzR' width='100%' height='600px'></iframe>
+  </main>
+</div>
+
+<div class="wrapper row4">
+  <footer id="footer" class="clear"> 
+    
+    <div class="one_half first">
+      <h6 class="title">The future is now</h6>
+      <img class="imgl pad5 borderedbox" style="height: 100px; width: 100px;" src="../images/demo/gallery/student.png" alt="Student">
+      <p>A journey of  thousand miles starts with one step. Lana will revolutionalize e-learning and bring
+        you the best in this field yet.
+      </p>
+      
+    </div>
+    <div class="one_quarter">
+      <h6 class="title">Contact us</h6>
+      <address class="push30">
+      Trend Ent<br>
+      250, 90119<br>
+      Matuu<br>
+           </address>
+      <ul class="nospace">
+        <li class="push10"><i class="icon-time"></i> Mon. - Fri. 9:00am - 7:00pm</li>
+        <li class="push10"><i class="icon-phone"></i> +254 797798449</li>
+        <li><i class="icon-envelope-alt"></i> lanalms.com</li>
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="title">Subscribe to our newsletter</h6>
+      <p>Get updates on any changes that are made on this platform realtime.</p>
+      <form action="#" method="post">
+        <label for="nlt_email">Mail <span>*</span></label>
+        <input class="push15" type="text" name="nlt_email" id="nlt_email" value="" size="22">
+        <button class="badge bold uppercase" type="submit" value="Subscribe">Subscribe <i class="icon-chevron-right"></i></button>
+      </form>
+    </div>
+    
+  </footer>
+</div>
+<div class="wrapper row5">
+  <div id="copyright" class="clear"> 
+    
+  </div>
+</div>
+</body>
+</html>
